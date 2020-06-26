affeae812bf20c8efde0bacd34f93a7707fd97f6
Mary Ondra <vidra48@mail.ru>
2020-06-26 17:53:35 +0300

    fun with properties

---
+++ AutoProps/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace AutoProps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fun with Automatic Properties");
+
+            Car c = new Car();
+            c.PetName = "Mo";
+            c.Speed = 60;
+            c.Color = "Red";
+
+            c.DisplayStats();
+
+            //add car into garage
+            Garage garage = new Garage();
+            garage.MyAuto = c;
+
+            //show amount of cars  in the garage
+            Console.WriteLine($"Number of cars in garage : {garage.NumberOfCars}");
+            Console.WriteLine($"Your car is named: {garage.MyAuto.PetName}");
+
+            Console.Read();
+        }
+    }
+
+    class Car
+    {
+        public string PetName { get; set; }
+        public int Speed { get; set; }
+        public string Color { get; set; }
+
+        public void DisplayStats() 
+        {
+            Console.WriteLine($"Car Name: {PetName}");
+            Console.WriteLine($"Speed: {Speed}");
+            Console.WriteLine($"Color: {Color}");
+        }
+
+    }
+
+    class Garage 
+    {
+        public int NumberOfCars { get; set; }
+        public Car MyAuto { get; set; }
+        public Garage()
+        {
+            MyAuto = new Car();
+            NumberOfCars = 1;
+        }
+        public Garage(Car car, int number)
+        {
+            MyAuto = car;
+            NumberOfCars = number;
+        }
+    }
+
+}
---
+++ ClassesPartTwo/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace ClassesPartTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fun with incapsulation\n");
+            Employee emp = new Employee("Marvin", 100, 456, 30000, "sdd");
+            emp.GiveBonus(1000);
+            emp.DisplayStats();
+
+            //use get/set to  change enployee name
+            emp.Name = "Marv";
+            Console.WriteLine($"Employee is named: {emp.Name}");
+
+            Employee joe = new Employee();
+            joe.Age++;       
+        }
+    } 
+
+    class Employee
+    {
+        //поля данных
+        private string empName;
+        private int empID;
+        private float currPay;
+
+        private string empSSN;
+
+        //properties// свойства
+
+        public string SocialSecurityNumber 
+        {
+            get { return empSSN; }
+        }
+
+
+        public string Name 
+        {
+            get { return empName; }
+            set 
+            {
+                if (value.Length > 15)
+                {
+                    Console.WriteLine("Error! Name lenght exceeds 15 characters!");
+                }
+                else
+                {
+                    empName = value;
+                }
+            }
+        }
+        public int Age { get; set; }
+        public int ID { get; set; }
+        public float Pay { get; set; }   
+
+        //constructors
+        public Employee() { }
+        public Employee(string name, int id, float pay)
+            : this(name, 0, id, pay, "") { }
+        public Employee(string name, int age, int id, float pay, string ssn) 
+        {
+            Name = name;
+            ID = id;
+            Age = age;
+            Pay = pay;
+            empSSN = ssn;
+        }
+
+
+        //methods
+        public void GiveBonus(float amount) 
+        {
+            currPay += amount;
+        }
+
+        public void DisplayStats() 
+        {
+            Console.WriteLine("Name: {0}", empName); //name of Employee
+            Console.WriteLine("ID: {0}", ID); //id of empoyee
+            Console.WriteLine("Age: {0}", Age); //age of empoyee
+            Console.WriteLine("Pay: {0}", currPay); //current pay
+        }
+    }
+}
