20647e9ed038558d67f693a967727c75d12faf06
Mary Ondra <vidra48@mail.ru>
2020-07-08 00:23:05 +0300

    finished with dynamic

---
+++ DynamicKeyword/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DynamicKeyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrinThreeStrings();
+
+            ChangeDynamicDataType();
+
+            InvokeMembersOnDynamicData();
+        }
+
+        static void PrinThreeStrings() 
+        {
+            var s1 = "Greetings";
+            object s2 = "From";
+            dynamic s3 = "Minneapolis";
+
+            Console.WriteLine($"s1 it type: {s1.GetType()}");
+            Console.WriteLine($"s2 it type: {s2.GetType()}");
+            Console.WriteLine($"s3 it type: {s3.GetType()}");
+        }
+
+        static void ChangeDynamicDataType() 
+        {
+            dynamic t = "Hello";
+            Console.WriteLine($"t is type: {t.GetType()}");
+
+            t = false;
+            Console.WriteLine($"t is type: {t.GetType()}");
+
+            t = new List<int>();
+            Console.WriteLine($"t is type: {t.GetType()}");
+        }
+
+        static void InvokeMembersOnDynamicData() 
+        {
+            dynamic dynamic1;
+            dynamic textData = "Hello";
+            Console.WriteLine(textData.ToUpper());
+
+           // Console.WriteLine(textData.toupper());
+           // Console.WriteLine(textData.Foo(10, "ee"));
+
+        }
+    }
+}
---
+++ FunWithDynamicAndAnonimus/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FunWithDynamicAndAnonimus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic[] dynamicArr = new dynamic [] {"Guide", 15, 15.5 };
+
+            dynamic[] dynamicArr2 = new dynamic [] {1000, 15, 1 };
+
+            dynamic[] dynamicArr3 = new dynamic [] {0, -15, 150 };
+
+            dynamic[] dynamicArr4 = new dynamic [] {'1', 'f', 'G' };
+        }
+    }
+}
