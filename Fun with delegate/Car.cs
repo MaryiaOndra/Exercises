7cca6018a18c38f893f5ccc6a3dc969ced57fc36
Mary Ondra <vidra48@mail.ru>
2020-07-23 17:39:44 +0300

    Fun with delegates: 1st part

---
+++ Fun with delegate/Car.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Fun_with_delegate
+{
+    class Car
+    {
+        //condition
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; } = 100;
+        public string PetName { get; set; }
+
+        //is car OK
+        private bool carIsDead;
+
+        //constractors
+        public Car()
+        {
+
+        }
+        public Car(string name, int maxSp, int currSp)
+        {
+            CurrentSpeed = currSp;
+            MaxSpeed = maxSp;
+            PetName = name;
+        }
+
+        public delegate void CarEngineHandler(string msgForCaller);
+
+        private CarEngineHandler listOfHandlers;
+
+        public void RegisterWithCarEngine(CarEngineHandler methodToCall) 
+        {
+            listOfHandlers += methodToCall;
+        }
+
+        public void UnRegisterWithCarEngine(CarEngineHandler methodToCall)
+        {
+            listOfHandlers -= methodToCall;
+        }
+
+        public void Accelerate(int delta) 
+        {
+            //if its broken send a message
+            if (carIsDead)
+            {
+                if (listOfHandlers != null)
+                {
+                    listOfHandlers("sorry, this car is dead...");
+                }
+            }
+            else
+            {
+                CurrentSpeed += delta;
+                //car is almost brocken
+                if (10 == (MaxSpeed - CurrentSpeed)
+                    && listOfHandlers != null)
+                {
+                    listOfHandlers("Careful buddy! Gonna blow!");
+                }
+
+                if (CurrentSpeed >= MaxSpeed)
+                {
+                    carIsDead = true;
+                }
+                else
+                {
+                    Console.WriteLine("Current Speed = {0}", CurrentSpeed);
+                }
+            }
+        }
+    }
+}
---
+++ Fun with delegate/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace Fun_with_delegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*******Delegates as event enablers*******\n");
+
+            //ceate Car
+            Car c1 = new Car("SlugBug", 100, 10);
+
+            //say to Car what method he shoud call to send a message
+            c1.RegisterWithCarEngine(new Car.CarEngineHandler(OnCarEngineEvent));
+
+            c1.RegisterWithCarEngine(new Car.CarEngineHandler(OnCarEngineEvent2));
+
+            //increase speed
+            Console.WriteLine("******Speeding up******");
+            for (int i = 0; i < 6; i++)
+            {
+                c1.Accelerate(20);
+            }
+
+            Console.ReadLine();
+        }
+
+        public static void OnCarEngineEvent(string msg ) 
+        {
+            Console.WriteLine("\n ******Message from car******");
+            Console.WriteLine("=> {0}", msg);
+            Console.WriteLine("*********************************");
+        }
+
+        public static void OnCarEngineEvent2(string msg) 
+        {
+            Console.WriteLine("=> {0}", msg.ToUpper());
+        }
+    }
+}
