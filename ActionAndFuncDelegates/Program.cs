6ee9394f41932587aa0f66087a1ff8dcf474f590
Mary Ondra <vidra48@mail.ru>
2020-07-24 17:50:46 +0300

    started work with lambda

---
+++ ActionAndFuncDelegates/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ActionAndFuncDelegates
+{
+    class Program
+    {
+        static void DisplayMessage(string msg, ConsoleColor txtColor, int printCount)
+        {
+            ConsoleColor previous = Console.ForegroundColor;
+            Console.ForegroundColor = txtColor;
+
+            for (int i = 0; i < printCount; i++)
+            {
+                Console.WriteLine(msg);
+            }
+
+            //return color
+            Console.ForegroundColor = previous;
+        }
+
+        static int Add(int x, int y) 
+        {
+            return y + x;
+        }
+        static string SumTostring(int x, int y) 
+        {
+            return (x + y).ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Action and Func *****");
+
+            //use Action<> for showing DisplayMessage
+            Action<string, ConsoleColor, int> actionTarget =
+                new Action<string, ConsoleColor, int>(DisplayMessage);
+            actionTarget("Action Message!", ConsoleColor.Yellow, 5);
+
+            Console.ReadLine();
+
+            //call Func methods
+
+            Func<int, int, int> funcTarget = Add;
+            int result = funcTarget.Invoke(40, 40);
+            Console.WriteLine("40 + 40 = {0}", result);
+
+            Func<int, int, string> funcTarget2 = SumTostring;
+            string sum = funcTarget2(90, 300);
+            Console.WriteLine(sum);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SimpleLambdaExpressions/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimpleLambdaExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Lambdas *****\n");
+            TraditionalDelagateSyntax();
+            Console.ReadLine();
+        }
+
+        static void TraditionalDelagateSyntax() 
+        {
+            List<int> list = new List<int>();
+            list.AddRange(new int[] { 20, 1, 4, 8, 9, 44 });
+
+            Predicate<int> callback = IsEvenNumber;
+            List<int> evenNumbers = list.FindAll(callback);
+
+            Console.WriteLine("Here are your even numbers:");
+
+            foreach (int  evenNum in evenNumbers)
+            {
+                Console.Write("{0}", evenNum);
+            }
+
+            Console.WriteLine();        
+        }
+
+        static bool IsEvenNumber(int i) 
+        {
+            return (i % 2).Equals(0);
+        }
+    }
+}
